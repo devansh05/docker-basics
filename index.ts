e45e3cfab66d0ea6d806e3649f19c5fdfd6c7005
devansh05 <devansh2393@gmail.com>
2026-06-21 16:20:10 +0530

    fixed pg and redis connections and added docker compose with basic configs

--- index.ts
+++ index.ts
@@ -4,38 +4,31 @@ import http from "http";
 import { Redis } from "ioredis";
 import { Pool } from "pg";
 
-const PORT = process.env.PORT || 3000;
-
-init();
-
 async function init() {
   try {
     console.log(`🟡 LOG - : REDIS`);
     // Redis Connection
-    console.log(`Connecting Redis...`);
-    const redis = new Redis("redis://redis:6379", { lazyConnect: true });
+    const redisHost = process.env.REDIS_HOST ?? "127.0.0.1";
+    const redisPort = process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 6379;
+    const redisUrl = process.env.REDIS_URL ?? `redis://${redisHost}:${redisPort}`;
+
+    console.log(`Connecting Redis at ${redisUrl}...`);
+    const redis = new Redis(redisUrl, {
+      lazyConnect: true,
+    });
+    redis.on("error", (error) => console.error("Redis error:", error));
     await redis.connect();
     console.log(`Redis Connection Success...`);
 
     // Postgresql Connection
     console.log(`Connecting Postgres...`);
 
-    // const { Client } = pg;
-    // const client = new Client({
-    //   host: "db",
-    //   port: 5432,
-    //   database: "postgres",
-    //   user: "postgres",
-    //   password: "postgres",
-    // });
-    // await client.connect();
-
     const pool = new Pool({
-      user: "postgres", // Your PostgreSQL username (default is 'postgres')
-      host: "localhost", // Server hosting your database
-      database: "postgres", // Name of the database you want to connect to
-      password: "mysecretpassword", // The password you set during Postgres installation
-      port: 5432, // Default PostgreSQL port
+      user: process.env.PG_USER ?? "postgres",
+      host: process.env.PG_HOST ?? "127.0.0.1",
+      database: process.env.PG_DATABASE ?? "postgres",
+      password: process.env.PG_PASSWORD ?? "postgres",
+      port: Number(process.env.PG_PORT ?? 5431),
     });
 
     await pool.connect();
@@ -43,7 +36,7 @@ async function init() {
     console.log(`Postgres Connection Success...`);
 
     // Http Server Stuff
-    const PORT = process.env.PORT ? +process.env.PORT : 8000;
+    const PORT = process.env.PORT ? +process.env.PORT : 3000;
     const server = http.createServer(app);
     server.listen(PORT, () =>
       console.log(`Http server is listening on PORT ${PORT}`),
@@ -53,6 +46,4 @@ async function init() {
   }
 }
 
-app.listen(PORT, () => {
-  console.log(`Server is live on http://localhost:${PORT}`);
-});
+init()
