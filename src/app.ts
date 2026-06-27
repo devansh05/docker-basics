f2b8041b77806f90f8fcb42cfec8cce20d0054aa
devansh05 <devansh2393@gmail.com>
2026-06-28 03:54:26 +0530

    added a health check service

--- src/app.ts
+++ src/app.ts
@@ -10,5 +10,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from src/app.ts!");
 });
 
+app.get("/health-ecs", (req, res) => {
+
+  res.json({message: "service is healthy."})
+})
+
 // Export the app instance as the default export
 export default app;
